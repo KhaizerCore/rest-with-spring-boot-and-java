869ec1fd05b6256804e0f924b3dd9a8cdd1389a8
KhaizerCore <gustavo.werkhaizer@hotmail.com>
2023-01-03 17:23:55 -0300

    adding 1 simple GET route greeting

---
+++ src/main/java/com/test/demo/Greeting.java
@@ -0,0 +1,19 @@
+package com.test.demo;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Greeting {
+    private final String name;
+    private final long id;
+    
+    public Greeting(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public long getId() {
+        return id;
+    }
+}
